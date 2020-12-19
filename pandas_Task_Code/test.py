bc32a67f066bbc96f16e42fb606a059fb0a4717d
Magicxiangye <30616925+Magicxiangye@users.noreply.github.com>
2020-12-19 21:44:36 +0800

    笔记3测试代码

--- pandas_Task_Code/test.py
+++ pandas_Task_Code/test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from numpy import number
 
 if __name__ == '__main__':
     # data = pd.Series([1,2,3])
@@ -8,5 +8,21 @@ if __name__ == '__main__':
     # back_data= data.shift(-1)
     # print(back_data)
     # print(data + back_data)
-    data = pd.Series([1, 2, 3])[::-1]
-    print(data.rolling(2).sum()[::-1])
+    # data = pd.Series([1, 2, 3])[::-1]
+    # print(data.rolling(2).sum()[::-1])
+    # s = pd.Series(['a', 'b', 'c', 'd', 'e', 'f'],
+    #               index=[1, 3, 1, 2, 5, 4])
+    # print(s[1:-1:1])
+    df = pd.read_csv('data/learn_pandas.csv')
+    #df_demo = df.set_index('Name')
+    #若要选出所有数值型的列
+    #布尔的选择列表
+    #print(df_demo.dtypes)
+    # condition_dtype = df.dtypes.isin(['int64','float64'])
+    # final_col = list(df.dtypes.loc[condition_dtype].index)
+    # print(df[final_col].head())
+    df_multi = df.set_index(['School', 'Grade'])
+    df_multi = df_multi.sort_index()
+    test = set(df_multi.index.values)
+    print(list(test))
+    print(df_multi.loc[list(test)[0:3]].head())
