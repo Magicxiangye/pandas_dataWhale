852c19b5fdd45fe8f34572428458986e357bdca1
Magicxiangye <30616925+Magicxiangye@users.noreply.github.com>
2020-12-21 21:50:23 +0800

    Task_03_Code

--- pandas_Task_Code/pandas_Task_03.py
+++ pandas_Task_Code/pandas_Task_03.py
@@ -33,6 +33,30 @@ def company():
     #修改索引名为原表名称
     #恢复默认索引并将列保持为原表的相对位置
     df_new.reindex_like(df)
+
+#EX2:巧克力数据集
+def chocolate():
+    df = pd.read_csv('data/chocolate.csv')
+    #把列索引名中的 \n 替换为空格
+    reset_index = df.index.map(lambda x: ' '.join(x.split('\n')))
+    df.index = reset_index
+    #巧克力 Rating 评分为1至5，每0.25分一档，请选出2.75分及以下且可可含量 Cocoa Percent 高于中位数的样本。
+    #x[:-1]切片去掉百分号
+    df['Cocoa Percent'] = df['Cocoa Percent'].apply(lambda x: float(x[:-1]) / 100).head()
+    #选符合条件的使用的是query方法
+    df.query("(Rating <= 2.75)&('Cocoa Percent' > 'Cocoa Percent'.median())").head()
+    #将 Review Date 和 Company Location 设为索引后，选出 Review Date
+    # 在2012年之后且 Company Location 不属于 France, Canada, Amsterdam, Belgium 的样本。
+    df.set_index(['Review Date', 'Company Location'])
+    #选出 Review Date在2012年之后
+    #多层索引的选择IndexSlice
+    idx = pd.IndexSlice
+    df.loc[idx[2012:,~df.index.get_level_values(1).isin(['France', 'Canada', 'Amsterdam', 'Belgium'])],:].head(3)
+
 #测试用
 if __name__ == '__main__':
-    pass
+    #company()
+    #chocolate()
+    df = pd.read_csv('data/chocolate.csv')
+    df['Cocoa Percent'] = df['Cocoa Percent'].apply(lambda x: print(x[:-1]))
+    print(df.head())
